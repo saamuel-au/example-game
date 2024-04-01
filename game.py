b2827d4961917e6cc2815792a02c025761b94491
Saamuel auvaart <saamuel.auvaart@voco.ee>
2024-04-01 11:20:11 +0300

    As a player i can see the game window [delivers #187304066]

--- game.py
+++ game.py
@@ -1,10 +1,18 @@
 import pygame
-pygame.init()
+from settings import Settings
 
-screen = pygame.display.set_mode([800, 500])
+pygame.init()
+gm_settings = Settings()
+
+
+
+screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+pygame.display.set_caption(gm_settings.caption)
 
 running = True
 while running:
+    
+    screen.fill(gm_settings.bg_color)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """Class for game settings"""
+    
+    def __init__(self):
+        """Initialize game settings"""
+        # Screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (0,0, 139)
+        self.caption = 'Bubble Bluster'
+        
+        
