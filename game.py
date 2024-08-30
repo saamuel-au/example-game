eef3cc415bf03ef3a714699d1835999ce0be4b2e
JakobKaur <147378697+JakobKaur@users.noreply.github.com>
2024-08-30 10:49:27 +0300

    game.py

--- game.py
+++ game.py
@@ -1,24 +1,40 @@
 import pygame
 from settings import Settings
-
-pygame.init()
-gm_settings = Settings()
-
-
-
-screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
-pygame.display.set_caption(gm_settings.caption)
-
-running = True
-while running:
+from button import Button
+from player import Player
+from bubble import Bubble
+from scoreboard import Scoreboard
+from game_stats import GameStats
+import game_functions as gf
+def run_game():
+    pygame.init()
+    gm_settings = Settings()
     
-    screen.fill(gm_settings.bg_color)
+    #Set up drawing window
+    screen = pygame.display.set_mode([gm_settings.screen_width, gm_settings.screen_height])
+    pygame.display.set_caption(gm_settings.caption)
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            
+    play_button = Button(gm_settings, screen, "Play")
     
-    pygame.display.flip()
+    stats = GameStats()
     
-pygame.quit()
+    sb = Scoreboard(gm_settings, screen, stats)
+    
+    clock = pygame.time.Clock()
+    
+    # Institate a player
+    player = Player(screen)
+    
+    # Creates groups to hold bubbles
+    bubbles = pygame.sprite.Group()
+    
+    # Run until user asks to quit
+    while True:
+        gf.check_events(gm_settings, screen, player, bubbles, stats, play_button)
+        if stats.game_active:
+            player.update()
+            gf.update_bubbles(player, bubbles, stats, sb)
+            gf.update_bubbles(player, bubbles, stats, sb, gm_settings)
+            bubbles.update()
+        else:
+            bubbles.empty()
